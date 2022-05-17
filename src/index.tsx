f49a5bc96a0f51a0b09a29ca8d2c532c98cf8ce0
Paulmgt <paul-maingault@outlook.fr>
2022-05-17 14:35:27 +0200

    Test1

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,103 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//on importe la librairie REACT
+import React from 'react'
+// on importe la librairie react-dom (permet d'afficher react sur la page)
+import { createRoot } from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const name: string = 'John week'
+// const id: string = 'ma-super-div'
+// const age: number = 32
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // On cree un element react ( du JSX)
+// const element = (
+//   // fragmet <> </>
+//   <>
+//     {/* <h3> h3 apprrait avant la div grace au fragment</h3> */}
+//     <div id={`${name}-${id}`} className="test">
+//       <h1> Bonjour tout le monde </h1>
+//       <p>Coucou {name}</p>
+//     </div>
+//   </>
+// )
+
+// const notes: Array<number> = [12, 15, 8, 9]
+// const age2: number = 16
+
+// const element2: JSX.Element = (
+//   // fragmet <> </>
+//   <>
+//     <div>
+//       <h1> Bonjour {name}</h1>
+//       <h2>Vos Notes</h2>
+//       <p>{age2 >= 18 ? 'Vous etes majeur' : 'Vous etes mineur'}</p>
+//       <ul>
+//         {notes.map((note, index) => (
+//           <li key={`note-${index}`}>{note}/ 20</li>
+//         ))}
+//       </ul>
+//     </div>
+//   </>
+// )
+
+type ObjetProps = {
+  name: string
+  age?: string
+}
+
+// ...................Fonction structurer.........................
+
+//  fontion (Hello) parametrer (props) : avec un objet (ObjetProps)
+// function Hello(props: ObjetProps): JSX.Element {
+//   return (
+//     <p>
+//       Hello Word{props.name}, vous avez {props.age} ans
+//     </p>
+//   )
+// }
+
+//............... Fonction Destructurer.......................
+
+function Hello({ name, age = 'XX' }: ObjetProps): JSX.Element {
+  return (
+    <p>
+      Hello {name}, vous avez {age} ans
+    </p>
+  )
+}
+type BigTextProps = {
+  children: React.ReactNode
+}
+function BigText({ children }: BigTextProps): JSX.Element {
+  return (
+    <div className="gros-text">
+      <h1>{children}</h1>
+    </div>
+  )
+}
+
+//  instentiation + affichage de la fonction Hello qui prend une balise perso <Hello>
+const element = (
+  <div>
+    <BigText>
+      Coucou
+      <br />
+      <span> nimporte quoi .......odjdkz</span>
+    </BigText>
+    <Hello name="Jean" />
+    <Hello name="Pierre" age="23" />
+    <Hello name="Jeane" age="34" />
+    <Hello name="Rosie" age="43" />
+  </div>
+)
+
+// on recupere la balise div#root de la page HTML
+const root = document.querySelector('#root')
+
+// Si il n'y a pas de balise root
+if (!root) {
+  // on leve l'erreur
+  throw new Error("Echec..! il n'y a pas de balise root dans votre index.html")
+}
+
+// J'affiche l'element à l'ecran dans la balise
+// div#root
+createRoot(root).render(element)
